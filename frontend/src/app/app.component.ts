db7fbd45366061b6be0c83cfaa20ec46d3676e81
Amy Hong <amyhong0502@gmail.com>
2019-02-12 06:40:28 -0800

    feat: get users and boats data from HTTP GET requests

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import users from './users';
-import boats from './boats';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
-  users = users;
-  boats = boats;
+export class AppComponent implements OnInit {
+  users: object;
+  boats: object;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Object>('../assets/boats.json').subscribe(res => {
+      this.boats = res;
+    });
+
+    this.http.get<Object>('../assets/users.json').subscribe(res => {
+      this.users = res;
+    });
+  }
 }
