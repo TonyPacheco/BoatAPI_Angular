a9fdec93238b492fdbf433d9533d1bef31a0c07d
Amy Hong <amyhong0502@gmail.com>
2019-02-12 14:57:43 -0800

    feat: implement boat-card component

--- frontend/src/app/boat-card/boat-card.component.ts
+++ frontend/src/app/boat-card/boat-card.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-boat-card',
   templateUrl: './boat-card.component.html',
-  styleUrls: ['./boat-card.component.css']
+  styleUrls: ['./boat-card.component.css'],
+  inputs: ['boat']
 })
 export class BoatCardComponent implements OnInit {
 
