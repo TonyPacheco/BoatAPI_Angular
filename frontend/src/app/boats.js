5f3335e65e2cf41cf39390840710f6a2b8e4d7f4
Amy Hong <amyhong0502@gmail.com>
2019-02-12 05:27:10 -0800

    chore: add seed data

---
+++ frontend/src/app/boats.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    boatId: 1,
+    boatName: 'Alpha Centauri',
+    picture: '',
+    lengthInFeet: 20,
+    make: 'Centaurus',
+    description: 'Lorem ipsum dolor sit amet, <strong>consectetur</strong> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <em>magna aliqua.</em>'
+  },
+  {
+    boatId: 2,
+    boatName: 'Proxima Centauri',
+    picture: '',
+    lengthInFeet: 30,
+    make: 'Centaurus',
+    description: 'Ut enim <em>ad minim veniam</em>, quis nostrud exercitation ullamco laboris nisi <sub>ut</sub> aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate <strong>velit esse cillum</strong> dolore eu fugiat nulla pariatur.'
+  },
+  {
+    boatId: 3,
+    boatName: 'Kochab',
+    picture: '',
+    lengthInFeet: 25,
+    make: 'Ursa Minor',
+    description: 'Excepteur sint occaecat cupidatat non proident, <mark>sunt in culpa qui officia</mark> deserunt mollit anim id est laborum.'
+  },
+  {
+    boatId: 4,
+    boatName: 'Callisto',
+    picture: '',
+    lengthInFeet: 17,
+    make: 'Jupiter',
+    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae <mark>vitae</mark> dicta sunt explicabo.'
+  },
+  {
+    boatId: 5,
+    boatName: 'Thuban',
+    picture: '',
+    lengthInFeet: 42,
+    make: 'Draco',
+    description: 'Nemo enim ipsam voluptatem <strong>quia voluptas sit aspernatur</strong> aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+  }
+];
