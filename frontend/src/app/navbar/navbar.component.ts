c69b95a0d021d96e6159346599780f40c56c3433
Amy Hong <amyhong0502@gmail.com>
2019-02-12 07:00:30 -0800

    feat: implement navigation bar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  title: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.title = 'A1';
   }
 
+  ngOnInit() {}
 }
