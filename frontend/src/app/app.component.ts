ef5fffbd26e1b0cb67dfb31b70767c9f48a7f71c
Amy Hong <amyhong0502@gmail.com>
2019-02-12 05:27:55 -0800

    feat: display seed data - users and boats

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import users from './users';
+import boats from './boats';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  users = users;
+  boats = boats;
 }
