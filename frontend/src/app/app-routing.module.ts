304dae1cac28e0b7701cf74f1cb2bc053106f584
Amy Hong <amyhong0502@gmail.com>
2019-02-23 18:49:33 -0800

    feat: add /boats route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { BoatsComponent } from './pages/boats/boats.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'boats', component: BoatsComponent },
   { path: '**', redirectTo: '/login' }
 ];
 
