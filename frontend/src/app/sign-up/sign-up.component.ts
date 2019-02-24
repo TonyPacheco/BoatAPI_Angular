96af37d40cb28d4c6050244c1d2690b6919bbd79
Amy Hong <amyhong0502@gmail.com>
2019-02-23 18:21:59 -0800

    feat: add signup function

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
+  ngOnInit() {}
+
+  private createForm(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  public submit(): void {
+    // TODO call authservice
+    const { firstName, lastName, email, password } = this.registerForm.value;
+  }
 }
