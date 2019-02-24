14fcfc833b8e65a3bc1792ce115ca4360f45bd89
Amy Hong <amyhong0502@gmail.com>
2019-02-23 19:29:19 -0800

    feat: add footer on /boats

--- frontend/src/app/pages/boats/boats.component.ts
+++ frontend/src/app/pages/boats/boats.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostBinding,
+  ChangeDetectorRef
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-boats',
   templateUrl: './boats.component.html',
-  styleUrls: ['./boats.component.css']
+  styles: [
+    `
+      :host(.footer-fixer) {
+        height: 100% !important;
+        -ms-flex-direction: column !important;
+        flex-direction: column !important;
+        display: -ms-flexbox !important;
+        display: flex !important;
+      }
+    `
+  ]
 })
 export class BoatsComponent implements OnInit {
   boats: object;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cdRef: ChangeDetectorRef) {}
+
+  @HostBinding('class.footer-fixer') footerFixer: boolean = false;
 
   ngOnInit(): void {
+    this.footerFixer = true;
+    this.cdRef.detectChanges();
+
     this.http.get<Object>('../assets/boats.json').subscribe(res => {
       this.boats = res;
     });
