b80835629ff075d96ac5aa06347a7418fa0cc694
Tony Pacheco <tonypacheco.cillis@gmail.com>
2019-02-25 16:58:25 -0800

    Move cors middleware

--- api/Startup.cs
+++ api/Startup.cs
@@ -89,8 +89,8 @@ namespace api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseCors("CORS");
+            app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
             DummyData.Initialize(app);
